9971af1a73284d2b7b9ba6857903875999181ac9
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-05-17 00:46:29 -0300

    Estrutura condicional

---
+++ Estrutura condicional/Exercício 5/Exercício 5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            int escolha, quantidade = 0;
+            float total = 0;
+
+            Console.WriteLine("Código | Especificação | Preço");
+            Console.WriteLine("1 | Cachorro quente | R$4,00");
+            Console.WriteLine("2 | X-Salada        | R$4,50");
+            Console.WriteLine("3 | X-Bacon         | R$5,00");
+            Console.WriteLine("4 | Torrada Simples | R$2,00");
+            Console.WriteLine("5 | Refrigerante    | R$1,50");
+
+            Console.WriteLine("\nEscolha seu produto:");
+            escolha = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEscolha a quantidade: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            switch (escolha) {
+
+                case 1:
+                    total = quantidade * 4f;
+                    break;
+
+                case 2:
+                    total = quantidade * 4.50f;
+                    break;
+
+                case 3:
+                    total = quantidade * 5f;
+                    break;
+
+                case 4:
+                    total = quantidade * 2f;
+                    break;
+
+                case 5:
+                    total = quantidade * 1.50f;
+                    break;
+
+                default:
+                    Console.WriteLine("\nOpção inválida!");
+                    break;
+            }
+
+            Console.WriteLine("\nTOTAL A PAGAR: R$" + total.ToString("F"));
+
+        }
+    }
+}
+
+/* Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+seguir, calcule e mostre o valor da conta a pagar. */
---
+++ Estrutura condicional/Exercício 6/Exercício 6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            double numero = 0;
+
+            Console.WriteLine("Digite um número: ");
+            numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (numero < 0 || numero > 100) {
+                Console.WriteLine("\nFora do intervalo!");
+            }else
+
+            if (numero <= 25) {
+                Console.WriteLine("\nIntervalo [0-25]");
+            }else
+
+            if (numero <= 50) {
+                Console.WriteLine("\nIntervalo [25-50]");
+            }else
+
+            if(numero <= 75) {
+                Console.WriteLine("\nIntervalo [50-75]");
+            }
+            else {
+                Console.WriteLine("\nIntervalo [75-100]");
+            }
+
+        }
+    }
+}
+
+/* Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”. */
