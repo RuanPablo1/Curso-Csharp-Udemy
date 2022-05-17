22af9650a5f7ec023e4061cc61c7000ba493ca10
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-05-17 00:42:00 -0300

    Estrutura condicional

---
+++ Estrutura condicional/Exercício 1/Exercício 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um número: ");
+            float num = float.Parse(Console.ReadLine());
+
+            if (num >= 0) {
+                Console.WriteLine("\nO número {0} não é negativo!", num);
+            }
+            else {
+                Console.WriteLine("\nO número {0} é negativo!", num);
+            }
+
+        }
+    }
+}
+
+//Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
