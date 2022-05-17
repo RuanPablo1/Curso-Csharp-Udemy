2818f6181ec445bfdd72d76eeab283bc0edb95d3
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-05-17 00:43:20 -0300

    Estrutura condicional

---
+++ Estrutura condicional/Exercício 2/Exercício 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um número para saber se ele é par ou ímpar: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0) {
+                Console.WriteLine("\nO número é par!");
+            }
+            else {
+                Console.WriteLine("\nO número é impar!");
+            }
+
+        }
+    }
+}
+
+//Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
