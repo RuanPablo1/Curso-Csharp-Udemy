635574a846a60a6e4ddf2c9e6d5750ce7cb9f326
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-05-17 00:44:25 -0300

    Estrutura condicional

---
+++ Estrutura condicional/Exercício 3/Exercício 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite 2 números inteiros: ");
+
+            string[] vetor = Console.ReadLine().Split(' ');
+            int numero1 = int.Parse(vetor[0]);
+            int numero2 = int.Parse(vetor[1]);
+
+            if (numero1 % numero2 == 0 && numero2 % numero1 == 0) {
+                Console.WriteLine("Os números são múltiplos entre si!");
+            }
+            else {
+                Console.WriteLine("Os números não são múltiplos!");
+            }
+        }
+    }
+}
+
+/* Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+ordem crescente ou decrescente. */
---
+++ Estrutura condicional/Exercício 4/Exercício 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            int horaInicio, horaFim, duracao = 0;
+
+            Console.WriteLine("Que horas começou o jogo?");
+            horaInicio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nQue horas acabou o jogo?");
+            horaFim = int.Parse(Console.ReadLine());
+
+
+            if (horaInicio < horaFim) {
+                duracao = horaFim - horaInicio;
+            }
+            else {
+                duracao = 24 - horaInicio + horaFim;
+            }
+
+            Console.WriteLine($"\nO jogo teve a duração de {duracao} hora(s)!");
+        }
+    }
+}
+
+/* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas. */
