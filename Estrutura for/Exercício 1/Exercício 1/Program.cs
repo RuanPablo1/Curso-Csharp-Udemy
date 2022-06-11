8717feee9372e92845c37f605cabe67afc6f8f54
Ruan Pablo <100460553+RuanPablo1@users.noreply.github.com>
2022-06-11 03:29:50 -0300

    Estrutura for

---
+++ Estrutura for/Exercício 1/Exercício 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            int i, numero_digitado = 0;
+
+            Console.WriteLine("Digite um número: ");
+            numero_digitado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nNúmero ímpares:");
+            for (i = 0; i <= numero_digitado; i++) {
+                if(i%2 != 0) {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
+
+/* Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso. */
---
+++ Estrutura for/Exercício 2/Exercício 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace nome_projeto {
+    internal class Program {
+
+        static void Main(string[] args) {
+
+            int numeros_digitados, fora = 0;
+            int quantidade_numeros = 0;
+            int dentro = 0;
+
+            Console.WriteLine("Quantos números quer digitar?");
+            quantidade_numeros = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidade_numeros; i++) {
+                numeros_digitados = int.Parse(Console.ReadLine());
+                if (numeros_digitados >= 10 && numeros_digitados <= 20) {
+                    dentro = dentro + 1;
+                }
+
+                else {
+                    fora = fora + 1;
+                }
+            }
+            Console.WriteLine("\nIn: " + dentro);
+            Console.WriteLine("Out: " + fora);
+        }
+    }
+}
+
+/* Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo). */
